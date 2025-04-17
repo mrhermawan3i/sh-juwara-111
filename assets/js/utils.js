bc26ea91c25f5f9ea04526c64d55bdecb79b84ac
fauzanalfaruqi <fauzanalfaruqi@gmail.com>
2025-04-17 12:20:50 +0700

    add functionality to daftar button

---
+++ assets/js/utils.js
@@ -0,0 +1,7 @@
+function goToWhatsapp() {
+    window.location.href = 'https://wa.me/6285283001487';
+}
+
+document.getElementById('wa-button').addEventListener('click', function() {
+  goToWhatsapp();
+});
